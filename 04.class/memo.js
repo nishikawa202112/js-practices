4c353abc51d4fc32c9d49bf980c3dc8ac99bcbd6
nishikawa <nishiyumn@jcom.zaq.ne.jp>
2022-10-24 23:29:30 +0900

    インジェクション対策等、修正しました

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -5,34 +5,85 @@ const fs = require("fs");
 const { Select } = require("enquirer");
 const arg = process.argv.slice(2);
 const MEMODB = "memodata.db";
+
 const memosclass = class {
   constructor() {
     const db = new sqlite3.Database(MEMODB);
     this.db = db;
   }
-  appendMemos(input) {
-    this.db.run("insert into memo(memo) values($memo)", {
-      $memo: input,
+  append(input) {
+    return new Promise(() => {
+      this.db.run("insert into memo(memo) values($memo)", {
+        $memo: input,
+      });
     });
   }
-  readMemos() {
-    return new Promise((resolve) => {
-      this.db.all("select id,memo from memo", (_err, rows) => {
+  async readAll() {
+    const rows_1 = await new Promise((resolve) => {
+      this.db.all("select id,memo from memo", (_error, rows) => {
         resolve(rows);
       });
     });
+    const memolines = rows_1.map((x) => `${x.id},${x.memo}`.split(","));
+    return memolines;
   }
-  readMemo(id) {
-    return new Promise((resolve) => {
-      this.db.get("select memo from memo where id =" + id, (_err, row) => {
+  async read(id) {
+    const row_1 = await new Promise((resolve) => {
+      const memo = this.db.prepare("select memo from memo where id=?");
+      memo.get(id, (_err, row) => {
         resolve(row);
       });
+      memo.finalize();
+    });
+    const memoLine = row_1.memo.split(",");
+    return memoLine;
+  }
+  delete(id) {
+    return new Promise(() => {
+      const memo = this.db.prepare("delete from memo where id=?");
+      memo.run(id);
+      memo.finalize();
     });
   }
-  deleteMemo(id) {
-    return new Promise(() => {
-      this.db.run("delete from memo where id=" + id);
+};
+
+const questionclass = class {
+  async read(memoLines) {
+    const prompt = await new Select({
+      name: "memo",
+      message: "choose a note you want to see:",
+      choices: memoLines.map((x) => ({ value: x[0], name: x[1] })),
+      result(name) {
+        return this.map(name);
+      },
     });
+    prompt
+      .run()
+      .then(async (value) => {
+        const answer = Object.entries(value);
+        const id = answer[0][1];
+        const memoLine = await memos.read(id);
+        console.log(memoLine.join("\n"));
+      })
+      .catch(console.error);
+  }
+  async delete(memoLines) {
+    const prompt = await new Select({
+      name: "memo",
+      message: "choose a note you want to delete:",
+      choices: memoLines.map((x) => ({ value: x[0], name: x[1] })),
+      result(name) {
+        return this.map(name);
+      },
+    });
+    prompt
+      .run()
+      .then(async (value) => {
+        const answer = Object.entries(value);
+        const id = answer[0][1];
+        await memos.delete(id);
+      })
+      .catch(console.error);
   }
 };
 
@@ -40,79 +91,23 @@ const memos = new memosclass();
 async function main() {
   if (arg[0] == null) {
     var input = fs.readFileSync("/dev/stdin", "utf8").trim().split("\n");
-    await append(input);
+    await memos.append(input);
   } else {
-    const memoLines = await read();
+    const memoLines = await memos.readAll();
     if (memoLines.length > 0) {
       if (arg[0] === "-l") {
         console.log(memoLines.map((x) => x[1]).join("\n"));
       } else if (arg[0] === "-r") {
-        const prompt = await new Select({
-          name: "memo",
-          message: "choose a note you want to see:",
-          choices: memoLines.map((x) => ({ value: x[0], name: x[1] })),
-          result(name) {
-            return this.map(name);
-          },
-        });
-        prompt
-          .run()
-          .then(async (value) => {
-            const answer = Object.entries(value);
-            const id = answer[0][1];
-            const memoLine = await getMemo(id);
-            console.log(memoLine.join("\n"));
-          })
-          .catch(console.error);
+        await new questionclass().read(memoLines);
       } else if (arg[0] === "-d") {
-        const prompt = await new Select({
-          name: "memo",
-          message: "choose a note you want to delete:",
-          choices: memoLines.map((x) => ({ value: x[0], name: x[1] })),
-          result(name) {
-            return this.map(name);
-          },
-        });
-        prompt
-          .run()
-          .then(async (value) => {
-            const answer = Object.entries(value);
-            const id = answer[0][1];
-            await delMemo(id);
-          })
-          .catch(console.error);
+        await new questionclass().delete(memoLines);
       }
     }
   }
 }
 
-async function append(input) {
-  memos.appendMemos(input);
-}
-
-async function read() {
-  const rows = await memos.readMemos();
-  const memoLines = rows.map((x) => `${x.id},${x.memo}`.split(","));
-  return memoLines;
-}
-
-async function getMemo(id) {
-  const row = await memos.readMemo(id);
-  const memoLine = row.memo.split(",");
-  return memoLine;
-}
-
-async function delMemo(id) {
-  await memos.deleteMemo(id);
-}
-
-function execMain() {
-  return new Promise(() => {
-    main();
-  });
-}
-
-(async () => {
-  await execMain();
+new Promise(() => {
+  main();
+}).then(() => {
   memos.db.close();
-})();
+});
